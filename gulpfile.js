cc3948db3177e1f50bd079faea21cfe08e2bfd6e
Riccardo Tagliavia <rtagliavia@gmail.com>
2018-08-14 14:42:26 -0500

    version 2
    Cambios en sponsors navbar social footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
-
+var cache = require('gulp-cache'); 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
@@ -10,13 +10,24 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
+
+gulp.task('clearCache', function() {
+
+  // Still pass the files to clear cache for
+  gulp.src('./lib/*.js')
+  .pipe(cache.clear());
+
+  // Or, just call this for everything
+  cache.clearAll();
+
+});
+
 // Move the javascript files into our /src/js folder
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
         .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
-
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
